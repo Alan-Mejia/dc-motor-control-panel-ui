6ce300054b183c17bc187c86e3d85bb2a0825690
Alan-Mejia <mejiamartinezalan@gmail.com>
2023-06-20 17:08:35 -0600

    Proyecto mejorado con PID

--- src/App.jsx
+++ src/App.jsx
@@ -15,32 +15,70 @@ function App() {
   const [animationDuration, setAnimationDuration] = useState("0ms")
   const [rpmsSelected, setRpmSelected] = useState(0)
 
+  const [maximumMotorRpm, setMaximumMotorRpm] = useState(0)
   const [rpmFromDevice, setRpmFromDevice] = useState(0)
+  const [maximumRpmFromDevice, setMaximumMotorRpmFromDevice] = useState(0)
   const [pinPwmOutputVoltage, setPinPwdOutputVoltage] = useState(0)
 
-  const handleOnClickRpm = ()=>{
-    const payload = {
-      from: `react-ui-${deviceName.current}`,
-      to: deviceName.current,
-      action: "changeRpm",
-      rpmDesired: rpmsSelected
-    }
+  const [kpSelected, setKpSelected] = useState(0.0)
+  const [kpFromDevice, setKpFromDevice] = useState(0.0)
+  const [kiSelected, setKiSelected] = useState(0.0)
+  const [kiFromDevice, setKiFromDevice] = useState(0.0)
+  const [kdSelected, setKdSelected] = useState(0.0)
+  const [kdFromDevice, setKdFromDevice] = useState(0.0)
 
-    stompClient.send(`/iot-websocket/change-rpm/${deviceName.current}`,{},JSON.stringify(payload))
-    setAnimationDuration("0ms")
+
+
+  const handleOnClickRpm = ()=>{
+
+    if(maximumMotorRpm >= rpmsSelected){
+      const payload = {
+        from: `react-ui-${deviceName.current}`,
+        to: deviceName.current,
+        action: "changeRpm",
+        rpmDesired: rpmsSelected,
+        maximumMotorRpm: maximumMotorRpm,
+        kp: kpSelected,
+        ki: kiSelected,
+        kd: kdSelected
+      }
+  
+      stompClient.send(`/iot-websocket/change-rpm/${deviceName.current}`,{},JSON.stringify(payload))
+      setAnimationDuration("0ms")
+    }else if(maximumMotorRpm < rpmsSelected && maximumMotorRpm>0){
+      const payload = {
+        from: `react-ui-${deviceName.current}`,
+        to: deviceName.current,
+        action: "changeRpm",
+        rpmDesired: 0,
+        maximumMotorRpm: maximumMotorRpm,
+        kp: kpSelected,
+        ki: kiSelected,
+        kd: kdSelected
+      }
+  
+      stompClient.send(`/iot-websocket/change-rpm/${deviceName.current}`,{},JSON.stringify(payload))
+      setAnimationDuration("0ms")
+    }/* else{
+      const payload = {
+        from: `react-ui-${deviceName.current}`,
+        to: deviceName.current,
+        action: "changeRpm",
+        rpmDesired: rpmsSelected,
+        maximumMotorRpm: 0
+      }
+  
+      stompClient.send(`/iot-websocket/change-rpm/${deviceName.current}`,{},JSON.stringify(payload))
+      setAnimationDuration("0ms")
+    } */
+
+    
 
   }
 
   const handleOnChangeRpm = (e)=>{
-    console.log(typeof e.target.value)
     const value = e.target.value === ''? 0 : parseInt(e.target.value)
-    if(value>7000){
-      setRpmSelected(7000)
-    }else if(value<0){
-      setRpmSelected(0)
-    }else{
-      setRpmSelected(value)
-    }
+    setRpmSelected(value)
   }
 
   const handleOnChangeUserName = (e)=>{
@@ -71,6 +109,10 @@ function App() {
     const messageContent = JSON.parse(payload.body)
     if(messageContent.from === deviceName.current && messageContent.to === `react-ui-${deviceName.current}`){
       setRpmFromDevice(messageContent.rpmWorking)
+      setMaximumMotorRpmFromDevice(messageContent.maximumMotorRpm)
+      setKdFromDevice(Number(messageContent.kd))
+      setKiFromDevice(messageContent.ki)
+      setKpFromDevice(messageContent.kp)
       const time = parseInt(60000/messageContent.rpmWorking)
       setAnimationDuration(`${time}ms`)
       setPinPwdOutputVoltage(messageContent.pinVoltageOutput)
@@ -86,7 +128,11 @@ function App() {
       from: `react-ui-${deviceName.current}`,
       to: deviceName.current,
       action: "changeRpm",
-      rpmDesired: 3500
+      rpmDesired: 2000, 
+      maximumMotorRpm: maximumMotorRpm,
+      kp: kpSelected,
+      ki: kiSelected,
+      kd: kdSelected
     }
     stompClient.send(`/iot-websocket/change-rpm/${deviceName.current}`,{},JSON.stringify(payload))
     setAnimationDuration("0ms")
@@ -98,11 +144,68 @@ function App() {
       from: `react-ui-${deviceName.current}`,
       to: deviceName.current,
       action: "changeRpm",
-      rpmDesired: 0
+      rpmDesired: 0,
+      maximumMotorRpm: maximumMotorRpm,
+      kp: kpSelected,
+        ki: kiSelected,
+        kd: kdSelected
     }
     stompClient.send(`/iot-websocket/change-rpm/${deviceName.current}`,{},JSON.stringify(payload))
     setAnimationDuration("0ms")
   }
+
+  const handleOnChangeMaximumRpm = (e)=>{
+    const value = e.target.value === ''? 0 : parseInt(e.target.value)
+    setMaximumMotorRpm(value)
+  }
+
+  const handleOnChangeKp = (e)=>{
+    const value = e.target.value === ''? 0.0 : Number(e.target.value)
+    setKpSelected(value)
+  }
+
+  const handleOnChangeKi = (e)=>{
+    const value = e.target.value === ''? 0.0 : Number(e.target.value)
+    setKiSelected(value)
+  }
+
+  const handleOnChangeKd = (e)=>{
+    const value = e.target.value === ''? 0.0 : Number(e.target.value)
+    setKdSelected(value)
+  }
+
+  const handleClickOnChangePid = ()=>{
+    if(maximumMotorRpm >= rpmsSelected){
+      const payload = {
+        from: `react-ui-${deviceName.current}`,
+        to: deviceName.current,
+        action: "changeRpm",
+        rpmDesired: rpmsSelected,
+        maximumMotorRpm: maximumMotorRpm,
+        kp: kpSelected,
+        ki: kiSelected,
+        kd: kdSelected
+      }
+  
+      stompClient.send(`/iot-websocket/change-rpm/${deviceName.current}`,{},JSON.stringify(payload))
+      setAnimationDuration("0ms")
+    }else if(maximumMotorRpm < rpmsSelected && maximumMotorRpm>0){
+      const payload = {
+        from: `react-ui-${deviceName.current}`,
+        to: deviceName.current,
+        action: "changeRpm",
+        rpmDesired: 0,
+        maximumMotorRpm: maximumMotorRpm,
+        kp: kpSelected,
+        ki: kiSelected,
+        kd: kdSelected
+      }
+  
+      stompClient.send(`/iot-websocket/change-rpm/${deviceName.current}`,{},JSON.stringify(payload))
+      setAnimationDuration("0ms")
+    }
+  }
+
 
   return (
     <>
@@ -136,19 +239,63 @@ function App() {
 
             <div className='inputs-container'>
               <div className='inputs-box'>
+                
+                <div style={{display: "flex", width: "80%", justifyContent: "space-around", height: "40px", alignItems: "center"}}>
+                  <label style={{fontSize: "18px", fontFamily: "sans-serif"}}>Set maximum motor RPM: </label>
+                  <input type='number' min={0} max={7000} defaultValue={rpmsSelected} onChange={handleOnChangeMaximumRpm}></input>
+                  <button onClick={handleOnClickRpm}>Accept</button>
+                </div>
+                
                 <div style={{display: "flex", width: "80%", justifyContent: "space-around", height: "40px", alignItems: "center"}}>
                   <label style={{fontSize: "18px", fontFamily: "sans-serif"}}>How Many RPM? </label>
                   <input type='number' min={0} max={7000} defaultValue={rpmsSelected} onChange={handleOnChangeRpm}></input>
                   <button onClick={handleOnClickRpm}>Accept</button>
                 </div>
 
+                
+
                 <div style={{fontSize: "20px", textAlign: "center", width: "100%", fontFamily: "sans-serif"}}>
+                  <p>{`Maximum RPM selected: ${maximumRpmFromDevice}`}</p>
                   <p>{`Current RPM working: ${rpmFromDevice}`}</p>
                   <p>{`PWM pin output voltage: ${pinPwmOutputVoltage}`}</p>
                 </div>
                 
               </div>
             </div>
+
+
+            <div className='inputs-container'>
+              <div className='inputs-box'>
+                
+                <div style={{display: "flex", width: "80%", justifyContent: "space-around", height: "40px", alignItems: "center"}}>
+                  <label style={{fontSize: "18px", fontFamily: "sans-serif"}}>Kp: </label>
+                  <input type='number' min={0} max={7000} defaultValue={kpSelected} onChange={handleOnChangeKp}></input>
+                  <button onClick={handleClickOnChangePid}>Accept</button>
+                </div>
+                
+                <div style={{display: "flex", width: "80%", justifyContent: "space-around", height: "40px", alignItems: "center"}}>
+                  <label style={{fontSize: "18px", fontFamily: "sans-serif"}}>Ki:  </label>
+                  <input type='number' min={0} max={7000} defaultValue={kiSelected} onChange={handleOnChangeKi}></input>
+                  <button onClick={handleClickOnChangePid}>Accept</button>
+                </div>
+                <div style={{display: "flex", width: "80%", justifyContent: "space-around", height: "40px", alignItems: "center"}}>
+                  <label style={{fontSize: "18px", fontFamily: "sans-serif"}}>Kd:  </label>
+                  <input type='number' min={0} max={7000} defaultValue={kdSelected} onChange={handleOnChangeKd}></input>
+                  <button onClick={handleClickOnChangePid}>Accept</button>
+                </div>
+
+                
+
+                <div style={{fontSize: "20px", textAlign: "center", width: "100%", fontFamily: "sans-serif"}}>
+                  <p>{`Kp selected: ${kpFromDevice}`}</p>
+                  <p>{`Ki selected: ${kiFromDevice}`}</p>
+                  <p>{`kd selected: ${kdFromDevice}`}</p>
+                </div>
+                
+              </div>
+            </div>
+
+
 
 
           </div>
